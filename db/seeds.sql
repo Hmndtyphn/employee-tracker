c106fdd6b8bc8776defbcfd772aa27314b8d24de
HmndTyphn <Jared.elliott09@gmail.com>
2021-10-31 23:01:21 -0500

    tables

---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES
+  ('Ronald', 'Firbank', 1, NULL),
+  ('Virginia', 'Woolf', 3, ),
+  ('Piers', 'Gaveston', 0, ),
+  ('Charles', 'LeRoi', 1, ),
+  ('Katherine', 'Mansfield', 1, ''),
+  ('Dora', 'Carrington', 0, ''),
+  ('Edward', 'Bellamy', 0, ''),
+  ('Montague', 'Summers', 1, ''),
+  ('Octavia', 'Butler', 1, ''),
+  ('Unica', 'Zurn', 1, '');
--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const mysql = require('mysql2');
 
 const express = require('express');
 
+const inquirer = require('inquirer');
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 
